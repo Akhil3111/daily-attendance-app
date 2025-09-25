575d97a98c9654c5b8e13acc9c327935f5476bdf
PALLAPU AKHIL <pallapuakhil_23it@student.vardhaman.org>
2025-09-25 13:52:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,6 @@ from flask import Flask, jsonify, request, send_from_directory
 from flask_cors import CORS
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
